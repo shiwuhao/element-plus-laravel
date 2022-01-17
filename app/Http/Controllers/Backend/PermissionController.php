abe5e1b9c0a2fd5524e65eb1a11918ac44f8adb7
shiwuhao <shiwuhao@juzishuke.com>
2022-01-17 17:08:10 +0800

    Permission

--- app/Http/Controllers/Backend/PermissionController.php
+++ app/Http/Controllers/Backend/PermissionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Exceptions\ApiException;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ApiResource;
 use App\Models\Permission;
@@ -11,10 +10,14 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 /**
- *
+ * 权限节点
  */
 class PermissionController extends Controller
 {
+    /**
+     * 排序step
+     * @var int
+     */
     protected $sortStep = 65536;
 
     /**
@@ -35,53 +38,108 @@ class PermissionController extends Controller
      */
     public function update(Request $request, Permission $permission): ApiResource
     {
-        $dropPermission = Permission::find($request->get('drop_id'));//3
-        $beforePermission = '';
-        $afterPermission = '';
+        $dropPermission = Permission::find($request->get('drop_id'));
 
-        if ($request->get('drop_type') == 'inner') {
-            $permission->pid = $dropPermission->id;
-        } elseif ($request->get('drop_type') == 'before') {
-            $beforePermission = Permission::wherePid($dropPermission->pid)->where('sort', '<', $dropPermission->sort)->latest('sort')->first(); // 5
-            if ($beforePermission) { // 存在两个之间
-                $permission->sort = ($dropPermission->sort + $beforePermission->sort) / 2;
-                $permission->pid = $beforePermission->pid;
-            } else {
-                $permission->sort = $dropPermission->sort / 2;
-                $permission->pid = $dropPermission->pid;
-            }
+        $permission = $this->getPermissionByDropType($permission, $dropPermission, $request);
 
-        } elseif ($request->get('drop_type') == 'after') {
-            $afterPermission = Permission::wherePid($dropPermission->pid)->where('sort', '>', $dropPermission->sort)->oldest('sort')->first();
-            if ($afterPermission) { // 存在两个之间
-                $permission->sort = ($dropPermission->sort + $afterPermission->sort) / 2;
-                $permission->pid = $afterPermission->pid;
-            } else {
-                $permission->sort = $dropPermission->sort + $this->sortStep;
-                $permission->pid = $dropPermission->pid;
-            }
-        }
-
-        if ($permission->sort == 0 || !is_int($permission->sort)) {
-            throw new ApiException('请先初始化排序后在进行操作');
+        if ($permission->sort == 0 || !is_int($permission->sort)) { //
+            $this->initPermissionSort();
+            $permission = $this->getPermissionByDropType($permission, $dropPermission, $request);
         }
 
         $permission->save();
 
-        return ApiResource::make(['before' => $beforePermission, 'after' => $afterPermission]);
+        return ApiResource::make(['message' => 'success']);
     }
 
     /**
-     * 初始化排序
-     * @return ApiResource
+     * 根据拖拽后节点的位置机选对应的排序值
+     * drop_type值为before、after、inner
+     * @param Permission $draggingPermission
+     * @param Permission $dropPermission
+     * @param Request $request
+     * @return Permission
      */
-    public function initSort(): ApiResource
+    protected function getPermissionByDropType(Permission $draggingPermission, Permission $dropPermission, Request $request): Permission
     {
-        Permission::oldest('sort')->oldest('id')->get()->each(function ($item, $key) {
+        switch ($request->get('drop_type')) {
+            case 'inner':
+                $draggingPermission = $this->getPermissionByInner($draggingPermission, $dropPermission);
+                break;
+            case 'before':
+                $draggingPermission = $this->getPermissionByBefore($draggingPermission, $dropPermission);
+                break;
+            case 'after':
+                $draggingPermission = $this->getPermissionByAfter($draggingPermission, $dropPermission);
+                break;
+        }
+
+        return $draggingPermission;
+    }
+
+    /**
+     * 基于前一个对象计算父级ID，排序标识
+     * @param Permission $draggingPermission
+     * @param Permission $dropPermission
+     * @return Permission
+     */
+    protected function getPermissionByInner(Permission $draggingPermission, Permission $dropPermission): Permission
+    {
+        $draggingPermission->pid = $dropPermission->id;
+
+        return $draggingPermission;
+    }
+
+    /**
+     * 基于前一个对象计算父级ID，排序标识
+     * @param Permission $draggingPermission
+     * @param Permission $dropPermission
+     * @return Permission
+     */
+    protected function getPermissionByBefore(Permission $draggingPermission, Permission $dropPermission): Permission
+    {
+        $beforePermission = Permission::wherePid($dropPermission->pid)->where('sort', '<', $dropPermission->sort)->latest('sort')->first(); // 5
+        if ($beforePermission) { // 放在在两个之间
+            $draggingPermission->sort = ($dropPermission->sort + $beforePermission->sort) / 2;
+            $draggingPermission->pid = $beforePermission->pid;
+        } else { // 放在第一个元素
+            $draggingPermission->sort = $dropPermission->sort / 2;
+            $draggingPermission->pid = $dropPermission->pid;
+        }
+
+        return $draggingPermission;
+    }
+
+    /**
+     * 基于后一个对象计算父级ID，排序标识
+     * @param Permission $draggingPermission
+     * @param Permission $dropPermission
+     * @return Permission
+     */
+    protected function getPermissionByAfter(Permission $draggingPermission, Permission $dropPermission): Permission
+    {
+        $afterPermission = Permission::wherePid($dropPermission->pid)->where('sort', '>', $dropPermission->sort)->oldest('sort')->first();
+        if ($afterPermission) { // 放在在两个之间
+            $draggingPermission->sort = ($dropPermission->sort + $afterPermission->sort) / 2;
+            $draggingPermission->pid = $afterPermission->pid;
+        } else { // 放在最后个元素
+            $draggingPermission->sort = $dropPermission->sort + $this->sortStep;
+            $draggingPermission->pid = $dropPermission->pid;
+        }
+
+        return $draggingPermission;
+    }
+
+    /**
+     * 初始化排序值
+     * @return Permission[]|Collection|\Illuminate\Support\Collection
+     */
+    protected function initPermissionSort()
+    {
+        return Permission::oldest('sort')->oldest('id')->get()->each(function ($item, $key) {
             $item->sort = ($key + 1) * $this->sortStep;
             $item->save();
         });
 
-        return ApiResource::make([]);
     }
 }
