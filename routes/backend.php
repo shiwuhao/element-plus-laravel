19e0f9484c8ed7e3e149bd46a60b2950200aab5a
shiwuhao <shiwuhao@juzishuke.com>
2022-01-21 10:38:36 +0800

    Permission

--- routes/backend.php
+++ routes/backend.php
@@ -8,33 +8,15 @@ use App\Http\Controllers\Backend\MenuController;
 use App\Http\Controllers\Backend\PermissionController;
 use App\Http\Controllers\Backend\PersonalController;
 use App\Http\Controllers\Backend\RoleController;
-use App\Http\Controllers\Backend\TestController;
 use App\Http\Controllers\Backend\UploadController;
 use App\Http\Controllers\Backend\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 
 Route::post('login', [LoginController::class, 'loginByPassword']);// 登录
 Route::post('logout', [LoginController::class, 'logout']);// 登出
+Route::post('/uploads', [UploadController::class, 'normal']);// 上传
 
-Route::post('/uploads', [UploadController::class, 'normal']);
-
-\Route::middleware(['auth:sanctum', 'permission'])->group(function () {
-    Route::get('test', [TestController::class, 'index']);
-});
 
 Route::prefix('configs')->group(function () {
     Route::get('items', [ConfigController::class, 'configItems']);
@@ -42,34 +24,30 @@ Route::prefix('configs')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    // 配置项
     Route::prefix('all')->group(function () {
         Route::get('menus', [AllController::class, 'menus']);
         Route::get('roles', [AllController::class, 'roles']);
         Route::get('permissions', [AllController::class, 'permissions']);
     });
 
+    // 个人信息
     Route::prefix('personal')->group(function () {
         Route::get('info', [PersonalController::class, 'info']);
         Route::get('permissions', [PersonalController::class, 'permissions']);
     });
 
+    // rbac鉴权
     Route::middleware('permission')->group(function () {
-        Route::prefix('configs')->group(function () {
-            Route::get('', [ConfigController::class, 'index']);
-            Route::post('', [ConfigController::class, 'store']);
-//            Route::get('items', [ConfigController::class, 'configItems']);
-            Route::get('group', [ConfigController::class, 'group']);
-            Route::put('group', [ConfigController::class, 'groupUpdate']);
-            Route::get('{config}', [ConfigController::class, 'show']);
-            Route::put('{config}', [ConfigController::class, 'update']);
-            Route::delete('{config}', [ConfigController::class, 'destroy']);
-        });
-
+        Route::get('configs/group', [ConfigController::class, 'group']);
+        Route::put('configs/group', [ConfigController::class, 'groupUpdate']);
+        Route::apiResource('configs', ConfigController::class);
 
         Route::apiResource('users', UserController::class);
         Route::apiResource('roles', RoleController::class);
         Route::apiResource('actions', ActionController::class);
         Route::apiResource('menus', MenuController::class);
+
         Route::post('/permissions/auto', [PermissionController::class, 'autoGenerate']);
         Route::apiResource('permissions', PermissionController::class);
 
